55e697f72bb2b00be9818faaa4466822f8bb49fd
Luiz Miguel Veloso <luizzmiguel17@gmail.com>
2024-03-07 15:46:59 -0300

    Publicando mudanças

--- site/assets/script/functions.js
+++ site/assets/script/functions.js
@@ -8,3 +8,17 @@ function menuShow() {
         document.querySelector('.icon').src = "/site/assets/mobile-icons/close_white_36dp.svg";
     }
 }
+
+function jogar_texto(doc_origem, id_destino) {
+    // fetch para buscar o conteúdo do header.html
+    fetch(doc_origem)
+        .then(response => response.text())
+        .then(data => {
+            // Colocando o conteúdo dentro do elemento com id de destino
+            document.getElementById(id_destino).innerHTML = data;
+        });
+}
+
+function teste(){
+    console.log("apenas um teste");
+}
