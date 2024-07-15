fb97a85a0e87c6ab0a9fe8740507ee5a8b2001ea
RodrigoAlSantos <rodrigochoradms@gmail.com>
2024-07-15 10:41:47 -0300

    testando contact_us banco de dados

--- contact-us/process_form.php
+++ contact-us/process_form.php
@@ -1,9 +1,8 @@
 <?php
-// Configuração do banco de dados
-$servername = "localhost"; // ou o IP do servidor
-$username = "root"; // usuário do banco de dados
-$password = ""; // senha do banco de dados
-$dbname = "contact_form"; // nome do banco de dados
+$servername = 'localhost';  // geralmente é 'localhost'
+$username = "contact_user"; // seu nome de usuário do banco de dados
+$password = "d>TDAf9[2I";    // sua senha do banco de dados
+$dbname = "contact_form";   // nome do seu banco de dados
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,19 +12,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Verifica se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Coleta os dados do formulário
-    $name = $conn->real_escape_string($_POST['name']);
-    $tel = $conn->real_escape_string($_POST['tel']);
-    $email = $conn->real_escape_string($_POST['email']);
-    $subject = $conn->real_escape_string($_POST['subject']);
-    $message = $conn->real_escape_string($_POST['message']);
+    $name = $_POST['name'];
+    $tel = $_POST['tel'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-    // Prepara a consulta SQL
-    $sql = "INSERT INTO usuarios (name, tel, email, subject, message) VALUES ('$name', '$tel', '$email', '$subject', '$message')";
+    $sql = "INSERT INTO contatos (name, tel, email, subject, message) VALUES ('$name', '$tel', '$email', '$subject', '$message')";
 
-    // Executa a consulta
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
@@ -33,6 +28,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Fecha a conexão
 $conn->close();
 ?>
