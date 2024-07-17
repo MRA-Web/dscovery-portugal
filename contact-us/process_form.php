0a906cc7200d59be6b1d8efb7b88d73a4ac9ada7
RodrigoAlSantos <rodrigochoradms@gmail.com>
2024-07-16 23:58:29 -0300

    up

--- contact-us/process_form.php
+++ contact-us/process_form.php
@@ -1,18 +1,10 @@
-
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+$servername = "localhost"; // O nome do servidor fornecido pela Hostinger
+$database = "u562265580_contact_form"; // Nome do banco de dados
+$username = "u562265580_contact_user"; // Nome de usuário do banco de dados
+$password = "N>UQhF8np5"; // Senha do banco de dados
 
-// Inclua o autoload do Composer
-require 'vendor/autoload.php';
-
-$servername = "localhost";
-$database = "u562265580_contact_form";
-$username = "u562265580_contact_user";
-$password = "N>UQhF8np5";
+header('Content-Type: application/json'); // Define o tipo de conteúdo como JSON
 
 // Cria a conexão
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -30,38 +22,14 @@ $email = mysqli_real_escape_string($conn, $_POST['email']);
 $subject = mysqli_real_escape_string($conn, $_POST['subject']);
 $message = mysqli_real_escape_string($conn, $_POST['message']);
 
-// Configuração do PHPMailer
-$mail = new PHPMailer(true);
-try {
-    // Configurações do servidor SMTP
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; 
-    $mail->SMTPAuth = true;
-    $mail->Username = 'dscoveryportugal@gmail.com';
-    $mail->Password = 'R7p!O2x@mB9z&T4X';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+// Cria a consulta SQL para inserir os dados
+$sql = "INSERT INTO contatos (name, tel, email, subject, message) VALUES ('$name', '$tel', '$email', '$subject', '$message')";
 
-    // Remetente e destinatário
-    $mail->setFrom('dscoveryportugal@gmail.com', 'Discovery Portugal');
-    $mail->addAddress('rodrigocowbr@gmail.com'); // E-mail do chefe
-    $mail->addAddress($email); // E-mail do cliente
-
-    // Assunto e corpo do e-mail
-    $mail->isHTML(true);
-    $mail->Subject = 'Dados do Formulário';
-    $mail->Body    = "<h1>Dados do Formulário</h1>
-                      <p><strong>Nome:</strong> $name</p>
-                      <p><strong>Telefone:</strong> $tel</p>
-                      <p><strong>Email:</strong> $email</p>
-                      <p><strong>Assunto:</strong> $subject</p>
-                      <p><strong>Mensagem:</strong> $message</p>";
-
-    // Enviando o e-mail
-    $mail->send();
-    echo json_encode(["status" => "success", "message" => "Dados enviados e e-mail notificado com sucesso!"]);
-} catch (Exception $e) {
-    echo json_encode(["status" => "error", "message" => "Não foi possível enviar o e-mail. Erro: {$mail->ErrorInfo}"]);
+// Executa a consulta
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error"]);
 }
 
 // Fecha a conexão
