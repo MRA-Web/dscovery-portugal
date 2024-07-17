213518c94e4848e80520ab1948f4f43cabe958db
RodrigoAlSantos <rodrigochoradms@gmail.com>
2024-07-17 18:26:57 -0300

    up

--- contact-us/form_script.js
+++ contact-us/form_script.js
@@ -18,3 +18,60 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     };
     xhr.send(formData);
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Inicialize o EmailJS com sua chave pública
+    emailjs.init('mH7Y-YFYHmYbEO9h7');  // Substitua pela sua chave pública
+
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        var form = document.getElementById("contactForm");
+        var nome = form.querySelector('input[name="name"]').value;
+        var telefone = form.querySelector('input[name="tel"]').value;
+        var email = form.querySelector('input[name="email"]').value;
+        var assunto = form.querySelector('input[name="subject"]').value;
+        var mensagem = form.querySelector('textarea[name="message"]').value;
+
+        generateAndSendPDF(nome, telefone, email, assunto, mensagem);
+    });
+
+    const { jsPDF } = window.jspdf;
+
+    function generateAndSendPDF(nome, telefone, email, assunto, mensagem) {
+        var doc = new jsPDF();
+        doc.text("Nome: " + nome, 10, 10);
+        doc.text("Telefone: " + telefone, 10, 20);
+        doc.text("Email: " + email, 10, 30);
+        doc.text("Assunto: " + assunto, 10, 40);
+        doc.text("Mensagem: " + mensagem, 10, 50);
+
+        var pdfBlob = doc.output('blob');
+        sendEmail(pdfBlob, nome, telefone, email, assunto, mensagem);
+    }
+
+    function sendEmail(pdfBlob, nome, telefone, email, assunto, mensagem) {
+        var reader = new FileReader();
+        reader.readAsDataURL(pdfBlob);
+        reader.onloadend = function() {
+            var base64data = reader.result.split(',')[1];
+
+            var templateParams = {
+                to_email: email,
+                name: nome,
+                tel: telefone,
+                subject: assunto,
+                message: mensagem,
+                pdf: base64data  // Se o template suportar anexos, isso deve funcionar
+            };
+
+            emailjs.send('service_9skp7pg', 'template_25j1jmr', templateParams)
+                .then(function(response) {
+                    console.log('Email enviado com sucesso!', response.status, response.text);
+                }, function(error) {
+                    console.error('Falha ao enviar o email.', error);
+                });
+        };
+    }
+});
