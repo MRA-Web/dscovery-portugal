3dd14d94399bd953368b56d2087dd063847aabfa
RodrigoAlSantos <rodrigochoradms@gmail.com>
2024-07-16 21:53:15 -0300

    teste 7 pdf email

--- contact-us/form_script.js
+++ contact-us/form_script.js
@@ -10,19 +10,17 @@ document.getElementById('contactForm').addEventListener('submit', function(event
                 const response = JSON.parse(xhr.responseText);
                 if (response.status === 'success') {
                     document.getElementById('contactForm').reset(); // Limpa os campos do formulário
-                    alert(response.message); // Exibe a mensagem de sucesso
+                    alert('Form submitted successfully!');
                 } else {
-                    alert('Error: ' + response.message); // Exibe a mensagem de erro
+                    alert('Error: ' + response.message);
                 }
             } catch (e) {
-                alert('Error parsing JSON response: ' + e.message); // Exibe erro de análise JSON
+                console.error('Error parsing JSON response:', e);
+                alert('Error parsing JSON response.');
             }
         } else {
-            alert('An error occurred while submitting the form. Status: ' + xhr.status); // Exibe erro com o status
+            alert('An error occurred while submitting the form.');
         }
-    };
-    xhr.onerror = function() {
-        alert('Request failed.'); // Mensagem de falha na requisição
     };
     xhr.send(formData);
 });
